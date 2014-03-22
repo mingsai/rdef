cc1d7bdac58b7633878089cc04076b9ec8be08c0
Tommie Carter <tcarternyc@hotmail.com>
2014-03-22 13:29:04 -0400

    Initial Commit

---
+++ rdef/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  rdef
+//
+//  Created by Tommie Carter on 22/3/14.
+//  Copyright (c) 2014 Tommie Carter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
