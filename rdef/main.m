984276913683e18c8d0c247299f6c073591fb791
Tommie Carter <tcarternyc@hotmail.com>
2014-03-24 01:56:15 -0400

    added CFRelease(def)

--- rdef/main.m
+++ rdef/main.m
@@ -12,27 +12,30 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
+
         if(argc < 2)
         {
             printf("Usage: rdef <word to define>");
 
-                   return -1;
-                   }
+            return -1;
+        }
 
-                   NSString * search =
-                   [NSString stringWithCString: argv[1] encoding: NSUTF8StringEncoding];
+        NSString * search =
+        [NSString stringWithCString: argv[1] encoding: NSUTF8StringEncoding];
 
-                   CFStringRef def =
-                   DCSCopyTextDefinition(NULL,
-                                         (__bridge CFStringRef)search,
-                                         CFRangeMake(0, [search length]));
-                   
-                   NSString * output = 
-                   [NSString stringWithFormat: @"Definition of <%@>: %@", search, (__bridge NSString *)def];
-                    
-                    printf("%s", [output UTF8String]);
-        
+        CFStringRef def =
+        DCSCopyTextDefinition(NULL,
+                              (__bridge CFStringRef)search,
+                              CFRangeMake(0, [search length]));
+
+        NSString * output =
+        [NSString stringWithFormat: @"Definition of <%@>: %@", search, (__bridge NSString *)def];
+
+        printf("%s", [output UTF8String]);
+
+        CFRelease(def);
+
+
     }
     return 0;
 }
